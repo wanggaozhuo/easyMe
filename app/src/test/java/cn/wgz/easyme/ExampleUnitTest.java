fe00e4712923e6b516695ad462889320c93bd66b
Wanggaozhuo <35704404@qq.com>
2018-09-28 21:45:52 +0800

    init commit

--- app/src/test/java/cn/wgz/easyme/ExampleUnitTest.java
+++ app/src/test/java/cn/wgz/easyme/ExampleUnitTest.java
@@ -4,14 +4,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- *
- * test istest 22222
- 0000000
- */
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
