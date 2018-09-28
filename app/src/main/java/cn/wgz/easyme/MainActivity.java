66887605a27b9c6a2fe92c652d63d4f9442d1593
Wanggaozhuo <35704404@qq.com>
2018-09-28 21:35:20 +0800

    Initial commit

---
+++ app/src/main/java/cn/wgz/easyme/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.wgz.easyme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
